3e0b143010491f75e8ce20306118b09ef49aea60
Nick Bansal <extreme_wired@msn.com>
2019-09-21 14:02:51 +0100

    socket io start

--- index.js
+++ index.js
@@ -1,57 +1,20 @@
-require('dotenv').config({ path: 'variables.env' });
-
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
-const Pusher = require('pusher');
-const NewsAPI = require('newsapi');
+const socketIo = require('socket.io');
+const axios = require('axios');
+
+
+const index = require('./routes');
 
 const app = express();
-
-const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-    encrypted: true,
-});
-
-const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
-
-const fetchNews = (searchTerm, pageNum) => newsapi.v2.everything({
-    q: searchTerm,
-    language: 'en',
-    page: pageNum,
-    pageSize: 5,
-});
-
+app.use('/', index);
 app.use(cors());
 
-function updateFeed(topic) {
-    let counter = 2;
-    setInterval(() => {
-        fetchNews(topic, counter)
-            .then((response) => {
-                console.log(response.articles);
-                pusher.trigger('news-channel', 'update-news', {
-                    articles: response.articles,
-                });
-                counter += 1;
-            })
-            .catch((error) => console.log(error));
-    }, 5000);
-}
 
-app.get('/live', (req, res) => {
-    const topic = 'bitcoin';
-    fetchNews(topic, 1)
-        .then((response) => {
-            res.json(response.articles);
-            updateFeed(topic);
-        })
-        .catch((error) => console.log(error));
-});
+const server = http.createServer(app);
+const io = socketIo(server); // < Interesting!
 
-app.set('port', process.env.PORT || 5000);
-const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-});
+const getApiAndEmit = 'TODO';
+
+module.exports = app;
---
+++ listen.js
@@ -0,0 +1,7 @@
+const app = require('.');
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Listening on server ${port} - http://localhost:8000/`);
+});
